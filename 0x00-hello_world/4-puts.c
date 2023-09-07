72ca7633c302666e0af67c0b22624292d6aeb4c7
NEXUS-0-1 <collinsernest049@gmail.com>
2023-09-07 15:37:38 -0700

    task-4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/*Program to print a sentnce using the printf() function*/
+#include <stdio.h>
+
+/**
+ * main -  this is the main function in the code
+ * void: tells us that the main function ccepts no argumnets
+ * Return: 0 if code is succesful
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
