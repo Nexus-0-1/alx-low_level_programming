4b46243557a20a76b6e5fa3f74f30dadccd27430
NEXUS-0-1 <collinsernest049@gmail.com>
2023-09-22 06:33:12 -0700

    This is the full pushing of every project in this repository

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - Entry point
+ * One IF statement, two loops only
+ * @n: string pointer
+ * Return: encrypted string
+ */
+
+char *rot13(char *n)
+{
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M'};
+
+	int i, j;
+
+	for (j = 0; n[j] != '\0'; j++)
+	{
+		i = 0;
+
+		while (a[i] != '\0' && n[j] != a[i])
+			i++;
+
+		if (n[j] == a[i])
+			n[j] = b[i];
+	}
+
+	return (n);
+}
